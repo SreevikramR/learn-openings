588c0ac7e2e66f3828cb8f2198e4265d3f2b72de
Sreevikram R <sreevikram.r@gmail.com>
2023-05-10 15:26:19 -0500

    Next updated, errorBox added- Login, Register page

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -13,6 +13,7 @@ const LoginForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        document.getElementById("errorBox").style.display = "none";
         setLoading(true);
         try {
             await setPersistence(auth, browserSessionPersistence).then(() =>
@@ -20,7 +21,7 @@ const LoginForm = () => {
             );
             router.push("/dashboard");
         } catch (error) {
-            //document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             switch (error.code) {
                 case "auth/user-not-found":
                     setErrorMessage("No Account found, try making one!");
@@ -40,6 +41,9 @@ const LoginForm = () => {
                 case "auth/too-many-requests":
                     setErrorMessage("Too many attemps, please try again later");
                     break;
+                case "auth/missing-password":
+                    setErrorMessage("Please enter a password");
+                    break;
                 default:
                     setErrorMessage("Code: " + error.code);
                     break;
@@ -50,25 +54,32 @@ const LoginForm = () => {
     };
 
     return (
-        <form className="flex flex-col text-2xl w-1/4">
-            <label className="pb-2">Email</label>
-            <input
-                type="email"
-                placeholder="name@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            /> 
-            <label className="pb-2 mt-6">Password</label>
-            <input
-                type="password"
-                placeholder="**********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            />
-            <button onClick={handleSubmit} disabled={loading} className="bg-blue-700 border-2 border-blue-700 text-white py-2 px-4 rounded-lg mt-10 hover:border-white disabled:bg-blue-500">Login</button>
-        </form>
+        <>
+            <form className="flex flex-col text-2xl w-1/4">
+                <div id='errorBox' className="flex place-content-center w-full p-3 bg-red-600 mb-4 text-xl border-2 border-red-600 rounded-md hidden">
+                    <text>{errorMessage}</text>
+                </div>
+                <label className="pb-2">Email</label>
+                <input
+                    type="email"
+                    placeholder="name@email.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                /> 
+                <label className="pb-2 mt-6">Password</label>
+                <input
+                    type="password"
+                    placeholder="**********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                />
+                <button onClick={handleSubmit} disabled={loading} className="bg-blue-700 border-2 border-blue-700 text-white py-2 px-4 rounded-lg mt-10 hover:border-white disabled:bg-blue-500">Login</button>
+            </form>
+            <text className="my-2 mt-3 font-semibold" onClick={() => router.push("/")}><span className="left-0 cursor-pointer text-lg">Forgot Password?</span></text>
+            <text className="text-lg font-semibold">New User? <span className="text-blue-500 cursor-pointer" onClick={() => router.push("/register")}>Register Here</span></text>
+        </>
     )
 }
 
--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -17,32 +17,33 @@ const RegisterForm = () => {
     const router = useRouter()
 
     const handleSubmit = async (e) => {
+        document.getElementById("errorBox").style.display = "none";
         setLoading(true);
         e.preventDefault();
         if (password !== passwordConfirm) {
-            // document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             setErrorMessage("Passwords don't match");
             setLoading(false);
         } else if (fName === "") {
-            // document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             setErrorMessage("Please enter a valid name");
             setLoading(false);
         } else if (lName === "") {
-            // document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             setErrorMessage("Please enter a valid name");
             setLoading(false);
         } else if (username === "") {
-            // document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             setErrorMessage("Please enter a valid username");
             setLoading(false);
         } else if (password === "") {
-            // document.getElementById("errorBox").style.display = "block";
+            document.getElementById("errorBox").style.display = "flex";
             setErrorMessage("Please enter a password");
             setLoading(false);
         } else {
             const doesExist = await checkUsernameExists(username);
             if (doesExist) {
-                // document.getElementById("errorBox").style.display = "block";
+                document.getElementById("errorBox").style.display = "flex";
                 setErrorMessage("Username already exists");
                 setLoading(false);
             } else {
@@ -55,7 +56,7 @@ const RegisterForm = () => {
                     await addUsername(username);
                     router.push("/dashboard");
                 } catch (error) {
-                    // document.getElementById("errorBox").style.display = "block";
+                    document.getElementById("errorBox").style.display = "flex";
                     switch (error.code) {
                         case "auth/email-already-exists":
                             setErrorMessage("Account with Email already exists");
@@ -75,6 +76,9 @@ const RegisterForm = () => {
                         case "auth/weak-password":
                             setErrorMessage("Please enter a longer password");
                             break;
+                        case "auth/missing-password":
+                            setErrorMessage("Please enter a password");
+                            break;
                         default:
                             setErrorMessage("Error code: " + error.code);
                             break;
@@ -87,63 +91,69 @@ const RegisterForm = () => {
     };
 
     return (
-        <form className="flex flex-col text-2xl w-1/4">
-            <div className="flex flex-row">
-                <div className="flex flex-col w-1/2 pr-2">
-                    <label className="pb-2 mt-2">First Name</label>
-                    <input
-                        type="Name"
-                        placeholder="First"
-                        value={fName}
-                        onChange={(e) => setFName(e.target.value)}
-                        className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-                    />
+        <>
+            <form className="flex flex-col text-2xl w-1/4">
+                <div id='errorBox' className="place-content-center w-full p-3 bg-red-600 mb-4 text-xl border-2 border-red-600 rounded-md hidden">
+                    <text>{errorMessage}</text>
                 </div>
-                <div className="flex flex-col w-1/2 pl-2">
-                    <label className="pb-2 mt-2">Last Name</label>
-                    <input
-                        type="name"
-                        placeholder="Last"
-                        value={lName}
-                        onChange={(e) => setLName(e.target.value)}
-                        className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-                    />
+                <div className="flex flex-row">
+                    <div className="flex flex-col w-1/2 pr-2">
+                        <label className="pb-2 mt-2">First Name</label>
+                        <input
+                            type="Name"
+                            placeholder="First"
+                            value={fName}
+                            onChange={(e) => setFName(e.target.value)}
+                            className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                        />
+                    </div>
+                    <div className="flex flex-col w-1/2 pl-2">
+                        <label className="pb-2 mt-2">Last Name</label>
+                        <input
+                            type="name"
+                            placeholder="Last"
+                            value={lName}
+                            onChange={(e) => setLName(e.target.value)}
+                            className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                        />
+                    </div>
                 </div>
-            </div>
-            <label className="pb-2 mt-6">Username</label>
-            <input
-                type="name"
-                placeholder="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            />
-            <label className="pb-2 mt-6">Email</label>
-            <input
-                type="email"
-                placeholder="name@mail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            />
-            <label className="pb-2 mt-6">Password</label>
-            <input
-                type="password"
-                placeholder="**********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            />
-            <label className="pb-2 mt-6">Confirm Password</label>
-            <input
-                type="password"
-                placeholder="**********"
-                value={passwordConfirm}
-                onChange={(e) => setPasswordConfirm(e.target.value)}
-                className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
-            />
-            <button onClick={handleSubmit} disabled={loading} className="bg-blue-700 border-2 border-blue-700 text-white py-2 px-4 rounded-lg mt-10 mb-10 hover:border-white disabled:bg-blue-500">Register</button>
-        </form>
+                <label className="pb-2 mt-6">Username</label>
+                <input
+                    type="name"
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                />
+                <label className="pb-2 mt-6">Email</label>
+                <input
+                    type="email"
+                    placeholder="name@mail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                />
+                <label className="pb-2 mt-6">Password</label>
+                <input
+                    type="password"
+                    placeholder="**********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                />
+                <label className="pb-2 mt-6">Confirm Password</label>
+                <input
+                    type="password"
+                    placeholder="**********"
+                    value={passwordConfirm}
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    className="bg-transparent border-0 border-b-2 border-b-white focus:outline-none pb-2"
+                />
+                <button onClick={handleSubmit} disabled={loading} className="bg-blue-700 border-2 border-blue-700 text-white py-2 px-4 rounded-lg mt-10 mb-3 hover:border-white disabled:bg-blue-500">Register</button>
+            </form>
+            <text className="mb-5 font-semibold text-lg">Have an Account?<span className="text-blue-500 cursor-pointer" onClick={() => router.push("/login")}> Login</span></text>
+        </>
     )
 }
 
