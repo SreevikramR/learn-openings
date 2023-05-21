9d4add3700531cba5190cc9bf991f8b303228b7e
Sreevikram R <sreevikram.r@gmail.com>
2023-05-20 21:16:28 -0500

    statCounter removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,17 +13,6 @@ export default function RootLayout({ children }) {
 		<html lang="en" className="h-full">
 			<ContextWrapper>
 				<body className="h-full">
-					<script type="text/javascript">
-						var sc_project=12883036; 
-						var sc_invisible=1; 
-						var sc_security="0bbd5ceb"; 
-					</script>
-					<script type="text/javascript"
-					src="https://www.statcounter.com/counter/counter.js" async></script>
-					<noscript><div class="statcounter"><a title="Web Analytics"
-						href="https://statcounter.com/" target="_blank"><img class="statcounter"
-						src="https://c.statcounter.com/12883036/0/0bbd5ceb/1/" alt="Web Analytics"
-					referrerPolicy="no-referrer-when-downgrade"/></a></div></noscript>
 					{children}
 				</body>
 			</ContextWrapper>
