0d5d1004841cca9be983105f05b4c28ebc48c88b
Sreevikram R <sreevikram.r@gmail.com>
2023-05-21 11:29:55 -0500

    GAnalytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import VercelAnalytics from '@/scripts/vercelAnalytics/VercelAnalytics'
 import './globals.css'
 import ContextWrapper from '@/components/wrapper/contextWrapper'
+import Script from 'next/script'
 
 export const metadata = {
 	title: 'Chess Openings',
@@ -13,6 +14,17 @@ export default function RootLayout({ children }) {
 		<html lang="en" className="h-full">
 			<ContextWrapper>
 				<body className="h-full">
+					<Script async strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-6KBPW0W9J9`}/>
+					<Script async id="ga-script" strategy="lazyOnload">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-6KBPW0W9J9', {
+						page_path: window.location.pathname,
+						});
+							`}
+					</Script>
 					{children}
 				</body>
 			</ContextWrapper>
