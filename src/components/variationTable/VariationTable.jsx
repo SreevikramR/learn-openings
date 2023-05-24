411ce3efaeb2bf88c259bde104bcbb8b4a522c88
Sreevikram R <sreevikram.r@gmail.com>
2023-05-24 16:57:32 -0500

    variations check mark added

--- src/components/variationTable/VariationTable.jsx
+++ src/components/variationTable/VariationTable.jsx
@@ -23,7 +23,7 @@ const VariationTable = () => {
         let row = (
             <tr key={lines[i]} onClick={() => setLine(line)} className={styles.variationRow}>
                 <td className={styles.td}>{lines[i]}</td>
-                <td className="w-1/4 text-right justify-center hidden flex-wrap content-center">&#10004;</td>
+                <td className="w-1/4 text-right justify-center flex flex-wrap content-center"><span className='text-black'>&#10004;</span><span className='text-white'>&#10004;</span></td>
             </tr>
         );
         rows.push(row)
