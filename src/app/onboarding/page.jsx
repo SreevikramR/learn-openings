ab2c2683a325c8a33d9bf663a212990392c38201
Sreevikram R <sreevikram.r@gmail.com>
2023-06-01 15:28:32 -0500

    onboarding page added

---
+++ src/app/onboarding/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const onBoardingPage = () => {
+	return (
+		<div>onBoardingPage</div>
+	)
+}
+
+export default onBoardingPage
---
+++ src/components/forms/OnboardingForm.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { useState } from 'react'
+
+const OnboardingForm = () => {
+    const [username, setUsername] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
+    
+
+    return (
+        <div>OnboardingForm</div>
+    )
+}
+
+export default OnboardingForm
